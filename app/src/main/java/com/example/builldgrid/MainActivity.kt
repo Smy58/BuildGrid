0ebf7e66a2539f95acaacec1d159978b730afb0a
Ilshat Osmanov <190103274@stu.sdu.edu.kz>
2023-11-11 03:27:46 +0600

    create: topic grid

--- app/src/main/java/com/example/builldgrid/MainActivity.kt
+++ app/src/main/java/com/example/builldgrid/MainActivity.kt
@@ -12,6 +12,9 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -26,6 +29,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.builldgrid.data.DataSource
 import com.example.builldgrid.model.Topic
 import com.example.builldgrid.ui.theme.BuilldGridTheme
 
@@ -39,7 +44,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TopicGrid(
+                        modifier = Modifier.padding(8.dp)
+                    )
                 }
             }
         }
@@ -54,6 +61,19 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun TopicGrid(modifier: Modifier = Modifier) {
+    LazyVerticalGrid(columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier
+    ){
+        items(DataSource.topics){ topic ->
+            TopicCard(topic = topic)
+
+        }
+    }
+}
 
 @Composable
 fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
@@ -71,6 +91,7 @@ fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
             Column {
                 Text(
                     text = stringResource(id = topic.name),
+                    fontSize = 14.sp,
                     modifier = Modifier.padding(
                         top = 16.dp,
                         start = 16.dp,
