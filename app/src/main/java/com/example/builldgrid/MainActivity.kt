2e08406eddfac9ed20c526d50ccdc37b08cc8e25
Ilshat Osmanov <190103274@stu.sdu.edu.kz>
2023-11-11 03:10:53 +0600

    create: topic card

--- app/src/main/java/com/example/builldgrid/MainActivity.kt
+++ app/src/main/java/com/example/builldgrid/MainActivity.kt
@@ -3,14 +3,29 @@ package com.example.builldgrid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.builldgrid.model.Topic
 import com.example.builldgrid.ui.theme.BuilldGridTheme
 
@@ -39,10 +54,42 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Composable
-fun TopicCard(topic: Topic, modifier: Modifier) {
-    Card{
 
+@Composable
+fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
+    Card{
+        Row {
+            Box {
+                Image(
+                    painter = painterResource(id = topic.imageRes),
+                    contentDescription = null,
+                    modifier = modifier.size(width = 68.dp, height = 68.dp),
+                    contentScale = ContentScale.Crop
+                )
+            }
+
+            Column {
+                Text(
+                    text = stringResource(id = topic.name),
+                    modifier = Modifier.padding(
+                        top = 16.dp,
+                        start = 16.dp,
+                        bottom = 8.dp,
+                        end = 16.dp
+                    )
+                )
+                Row( verticalAlignment = Alignment.CenterVertically ) {
+                    Icon(painter = painterResource(R.drawable.ic_grain), contentDescription = null,
+                        modifier = Modifier.padding(start = 16.dp)
+                    )
+                    Text(
+                        text = topic.courses.toString(),
+                        modifier = Modifier.padding(start = 8.dp)
+                    )
+                    
+                }
+            }
+        }
     }
 }
 
@@ -50,6 +97,14 @@ fun TopicCard(topic: Topic, modifier: Modifier) {
 @Composable
 fun GreetingPreview() {
     BuilldGridTheme {
-        Greeting("Android")
+        val someTopic = Topic(R.string.architecture, 123, R.drawable.architecture)
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+
+        ) {
+            TopicCard(topic = someTopic)
+        }
     }
 }
